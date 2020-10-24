d44ef7ffabaf46425a98de1d50d3eb90b0222810
Troy Good <troygood13@gmail.com>
2020-10-23 19:03:27 -0700

    Initial commit

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using UnityEngine;
+
+public class Cube : Agent
+{
+    [SerializeField] public float sidewaysForce = 100f;
+    [SerializeField] private KeyCode leftKey;
+    [SerializeField] private KeyCode rightKey;
+
+    private Rigidbody rBody;
+    private Vector3 startingPosition;
+    //private int score = 0;
+
+    public ObstacleSpawner obstacleSpawner;
+
+    public GameObject ground;
+
+    public event Action OnReset;
+
+    public override void Initialize()
+    {
+        rBody = GetComponent<Rigidbody>();
+        startingPosition = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (transform.position.x < ground.transform.position.y)
+        {
+            AddReward(increment: 0.1f);
+            EndEpisode();
+        }
+
+        RequestDecision();
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        if (Mathf.FloorToInt(vectorAction[0]) == 1)
+        {
+            MoveLeft();
+        }
+        if (Mathf.FloorToInt(vectorAction[1]) == 1)
+        {
+            MoveRight();
+        }
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        Reset();
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = 0;
+        actionsOut[1] = 0;
+
+        if (Input.GetKey(leftKey))
+        {
+            MoveLeft();
+            actionsOut[0] = 1;
+        }
+
+        if (Input.GetKey(rightKey))
+        {
+            MoveRight();
+            actionsOut[1] = 1;
+        }
+    }
+
+    private void MoveLeft()
+    {
+        rBody.AddForce(-sidewaysForce * Time.fixedDeltaTime, 0, 0, ForceMode.VelocityChange);
+        //Vector3 position = transform.position;
+        //position.x -= .1f;
+        //transform.position = position;
+
+    }
+
+    private void MoveRight()
+    {
+        rBody.AddForce(sidewaysForce * Time.fixedDeltaTime, 0, 0, ForceMode.VelocityChange);
+        //Vector3 position = transform.position;
+        //position.x += .1f;
+        //transform.position = position;
+    }
+
+    private void Reset()
+    {
+        //score = 0;
+
+        //transform.position = startingPosition;
+
+        rBody.velocity = Vector3.zero;
+
+        obstacleSpawner.RemoveObstacles();
+
+        OnReset?.Invoke();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle")) 
+        {
+            AddReward(increment: -1.0f);
+            EndEpisode();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Score"))
+        {
+            AddReward(increment: 1.0f);
+            //score++;
+        }
+    }
+}
---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+
+    public GameObject obstaclePrefab;
+
+    public GameObject scorePrefab;
+
+    public GameObject cube;
+
+    public float forwardForce = 20f;
+
+    public float timeBetweenWaves = 1f;
+
+    private List<GameObject> obstacles = new List<GameObject>();
+
+    public void SpawnObstacles()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (randomIndex != i)
+            {
+                GameObject ob = Instantiate(obstaclePrefab, spawnPoints[i].position, Quaternion.identity);
+                ob.tag = "Obstacle";
+                obstacles.Add(ob);
+            } else
+            {
+                GameObject ob = Instantiate(scorePrefab, spawnPoints[i].position, Quaternion.identity);
+                ob.tag = "Score";
+                obstacles.Add(ob);
+            }
+        }
+    }
+
+    public void RemoveObstacles()
+    {
+        for (int i = 0; i < obstacles.Count; i++)
+        {
+            if (obstacles[i])
+            {
+                Destroy(obstacles[i]);
+            }
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacles", 0f, timeBetweenWaves);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+
+    {
+        for (int i = 0; i < obstacles.Count; i++)
+        {
+            // Add constant force to obstacle
+            if (obstacles[i])
+            {
+                obstacles[i].GetComponent<Rigidbody>().AddForce(0, 0, -forwardForce * Time.fixedDeltaTime, ForceMode.VelocityChange);
+
+                // Remove obstacle if past player
+                if (obstacles[i].transform.position.z < cube.transform.position.z-10)
+                {
+                    Destroy(obstacles[i]);
+                }
+
+            }
+        }
+
+    }
+}
